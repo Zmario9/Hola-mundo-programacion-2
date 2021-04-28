fadc1f472704b08aa3bdd98def39136971ae56b8
Zmario9 <83045811+Zmario9@users.noreply.github.com>
2021-04-28 13:56:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello prgr2!!");
+        }
+    }
+}
